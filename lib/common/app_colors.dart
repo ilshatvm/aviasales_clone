401f4f50becbf6ac49943ead9c5ef1e51ca4f416
Ilshat <ilshatvm13@gmail.com>
2024-05-15 21:13:11 +0500

    added app colors

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color black = Color(0xFF0C0C0C);
+  static const Color grey1 = Color(0xFF1D1E20);
+  static const Color grey2 = Color(0xFF242529);
+  static const Color grey3 = Color(0xFF2F3035);
+  static const Color grey4 = Color(0xFF3E3F43);
+  static const Color grey5 = Color(0xFF5E5F61);
+  static const Color grey6 = Color(0xFF9F9F9F);
+  static const Color grey7 = Color(0xFFDBDBDB);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color blue = Color(0xFF2261BC);
+  static const Color blueLight = Color(0xFF4C95FE);
+  static const Color blueDark = Color(0xFF00427D);
+
+  static const Color green = Color(0xFF3A633B);
+  static const Color greenLight = Color(0xFF4CB24E);
+  static const Color greenDark = Color(0xFF1E341F);
+
+  static const Color red = Color(0xFFFF5D5D);
+
+  static const Color orange = Color(0xFFF36E36);
+}
